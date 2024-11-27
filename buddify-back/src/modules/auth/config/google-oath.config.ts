0d3545c5aec17da9d2a31ace4bfe3c5bc7e576ba
Lisandro85 <lisandrobedotti@hotmail.com>
2024-11-27 08:58:40 +0100

    verselDeploy

--- buddify-back/src/modules/auth/config/google-oath.config.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from 'dotenv';
-import { registerAs } from '@nestjs/config';
-dotenv.config({path:'./.env.local'});
-
-export default registerAs("googleOAuth",()=>({
-    clientID: process.env.GOOGLE_CLIENTE_ID,
-    clientSecret:process.env.GOOGLE_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL,
-}));
